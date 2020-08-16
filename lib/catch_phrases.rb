cae5282167342f45984ca2a23615ef4972655238
Jason Easterly <jason.easterly11@gmail.com>
2020-08-16 19:02:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,19 +1,16 @@
 def mario
   phrase = 'It\'s-me, Mario!'
   puts phrase
-  phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  status
 end
 
 def link
   warning = "It's Dangerous To Go Alone! Take This"
   puts warning
-  warning
 end
 
 def all_phrases
